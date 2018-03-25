d67215473ec198e171f1168f114a9f9ed6d5e807
djpluki <djpluki@gmail.com>
2018-03-25 20:44:16 +0200

    create not optimized mobile verion of listin page

--- js/view-switcher.js
+++ js/view-switcher.js
@@ -1,15 +1,31 @@
-const btn = document.getElementById('view-switcher');
+const btnVS = document.getElementById('view-switcher');
 const mapContainer = document.getElementById('map-container');
 let mapVisible = false;
 
-btn.addEventListener('click', () => {
+const btnFilter = document.getElementById('view-switcher-filter');
+const closeBtn = document.getElementById('filter-close');
+const filterOptions = document.getElementById('filter-options');
+
+btnVS.addEventListener('click', () => {
   if (!mapVisible) {
     mapContainer.classList.add('content__left--visible');
     mapVisible = true;
-    btn.innerText = 'list';
+    btnVS.innerHTML = `
+    <i class="fas fa-table fa-lg icon"></i>
+    `;
   } else {
     mapContainer.classList.remove('content__left--visible');
     mapVisible = false;
-    btn.innerText = 'map';
+    btnVS.innerHTML = `
+    <i class="fas fa-map fa-lg icon"></i>
+    `;
   }
+})
+
+btnFilter.addEventListener('click', () => {
+  filterOptions.classList.add('filter-options--expanded');
+})
+
+closeBtn.addEventListener('click', () => {
+  filterOptions.classList.remove('filter-options--expanded');
 })
